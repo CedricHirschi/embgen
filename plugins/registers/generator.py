5be94c814f0cc81dec321fae8abdade0f2bced29
Cédric Hirschi <cedrichirschi.21@gmail.com>
2026-08-23 14:49:14 +0200

    Add PeakRDL generator

--- plugins/registers/generator.py
+++ plugins/registers/generator.py
@@ -1,8 +1,9 @@
+from enum import Enum
 from pathlib import Path
 
 from embgen.plugin import GeneratedFile, Generator
 
-from .schema import RegmapSchema
+from .schema import Access, RegmapSchema
 
 
 class RegmapGenerator(Generator[RegmapSchema]):
@@ -26,6 +27,101 @@ class RegmapGenerator(Generator[RegmapSchema]):
 
         return c
 
+    def _generate_rdl(self, input: RegmapSchema) -> str:
+        c = ""
+
+        c += f"addrmap {input.name} {{\n"
+        c += f'    name = "{input.name}";\n'
+        if input.description:
+            c += f'    desc = "{input.description}";\n'
+        c += "\n"
+
+        c += f"    default regwidth = {input.width};\n"
+        c += "    default sw = rw;\n"
+        c += "    default hw = rw;\n\n"
+
+        current_register_address = 0
+
+        for register in input.registers:
+            c += "    reg {\n"
+            if register.description:
+                c += f'        desc = "{register.description}";\n'
+            c += "\n"
+
+            added_defaults = False
+            if register.access is not None and register.access != Access.RW:
+                c += f"        default sw = {register.access.value};\n"
+                added_defaults = True
+            if register.access_hw is not None and register.access_hw != Access.RW:
+                c += f"        default hw = {register.access_hw.value};\n"
+                added_defaults = True
+            if added_defaults:
+                c += "\n"
+
+            current_bitfield_offset = 0
+
+            for bitfield in register.bitfields:
+                c += "        field {\n"
+                if bitfield.description:
+                    c += f'            desc = "{bitfield.description}";\n'
+
+                enum_name = None
+
+                if bitfield.enums is not None:
+                    enum_name = f"{register.name.lower()}_{bitfield.name.lower()}_e"
+                    c += f"\n            enum {enum_name} {{\n"
+                    for enum in bitfield.enums:
+                        if enum.description is None:
+                            c += f"                {enum.name} = {bitfield.width}'d{enum.value};\n"
+                        else:
+                            c += f"                {enum.name} = {bitfield.width}'d{enum.value} {{ "
+                            c += f'desc = "{enum.description}";'
+                            c += " };\n"
+                    c += "            };\n\n"
+
+                if bitfield.reset is not None:
+                    if isinstance(bitfield.reset, int):
+                        if bitfield.enums is None:
+                            c += f"            reset = {bitfield.width}'d{bitfield.reset:d};\n"
+                        else:
+                            reset_name = None
+                            for enum in bitfield.enums:
+                                if enum.value == bitfield.reset:
+                                    reset_name = enum.name
+                                    break
+                            if reset_name is not None:
+                                c += f"            reset = {enum_name}::{reset_name};\n"
+                            else:
+                                c += f"            reset = {bitfield.width}'d{bitfield.reset:d};\n"
+                    elif isinstance(bitfield.reset, Enum):
+                        c += f"            reset = {bitfield.reset.name};\n"
+                    elif isinstance(bitfield.reset, bool):
+                        c += f"            reset = {bitfield.width}'d{'1' if bitfield.reset else '0'};\n"
+
+                if bitfield.enums is not None:
+                    c += f"            encode = {register.name.lower()}_{bitfield.name.lower()}_e;\n"
+
+                offset = (
+                    bitfield.offset
+                    if bitfield.offset is not None
+                    else current_bitfield_offset
+                )
+                msb = offset + bitfield.width - 1
+                lsb = offset
+
+                c += f"        }} {bitfield.name} [{msb}:{lsb}];\n\n"
+
+            if register.address is not None:
+                c += f"    }} {register.name} @ 0x{register.address:X};\n\n"
+                current_register_address = register.address + (input.width // 8)
+            else:
+                c += f"    }} {register.name} @ 0x{current_register_address:X};\n\n"
+                current_register_address += input.width // 8
+
+        c += "};\n\n"
+
+        return c
+
     def generate(self, input: RegmapSchema) -> list[GeneratedFile]:
         result = []
 
@@ -35,5 +131,10 @@ class RegmapGenerator(Generator[RegmapSchema]):
                     content = self._generate_md(input)
                     result.append(
                         GeneratedFile(path=Path("regmap.md"), content=content)
+                    )
+                case ".rdl":
+                    content = self._generate_rdl(input)
+                    result.append(
+                        GeneratedFile(path=Path("regmap.rdl"), content=content)
                     )
         return result
--- plugins/registers/schema.py
+++ plugins/registers/schema.py
@@ -1,29 +1,80 @@
+from enum import Enum as BaseEnum
 from typing import Literal, Optional
+
+from pydantic import model_validator
 
 from embgen.common import StrictModel
 from embgen.plugin import Schema
 
-RegmapExtension = Literal[".md", ".h", ".py"]
+RegmapExtension = Literal[".md", ".rdl"]
+
+
+class Access(BaseEnum):
+    """Register access type."""
+
+    RO = "r"
+    RW = "rw"
+    WO = "w"
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __repr__(self) -> str:
+        return "Access." + str(self)
+
+
+class Enum(StrictModel):
+    """Enumeration value used in both commands and registers."""
+
+    name: str
+    description: str | None = None
+    value: int
 
 
 class BitField(StrictModel):
     name: str
     description: Optional[str] = None
-    reset: int
-    width: int
-    offset: int
+    reset: int | bool | Enum | str
+    width: int = 1
+    offset: Optional[int] = None
+    enums: Optional[list[Enum]] = None
+
+    @model_validator(mode="after")
+    def validate_reset(self) -> "BitField":
+        if self.enums is not None:
+            if isinstance(self.reset, str):
+                found = False
+                for enum in self.enums:
+                    if enum.name == self.reset:
+                        self.reset = enum.value
+                        found = True
+                        break
+
+                if not found:
+                    raise ValueError(
+                        f"BitField {self.name} has invalid reset enum value: {self.reset}. Available enums: {[e.name for e in self.enums]}"
+                    )
+        return self
 
 
 class Register(StrictModel):
     name: str
     description: Optional[str] = None
-    address: int
+    address: Optional[int] = None
     bitfields: list[BitField]
+    access: Access = Access.RW
+    access_hw: Optional[Access] = None
+
+    # regtool specific fields (See https://opentitan.org/book/util/reggen/index.html)
+    hwqe: bool = False  # If hardware uses 'q' enable signal, which is latched signal of software write pulse
+    hwext: bool = False  # If the register is stored outside of the register module
 
 
 class RegmapSchema(Schema):
     name: str
     description: Optional[str] = None
+    width: int = 32
+    access_separate: bool = False
 
     extensions: list[RegmapExtension]
     registers: list[Register]
